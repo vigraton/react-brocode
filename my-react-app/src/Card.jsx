efb06f3dd3a9cc7424841ffa511b06e3c5bc56d9
vigraton <viviangraton@gmail.com>
2024-10-25 13:13:10 -0300

    initial commit

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,13 @@
+import profilePic from './assets/tired-jake.jpg'
+
+function Card(){
+    return(
+        <div className="card">
+            <img className="card-image" src={profilePic} alt="profile picture" height={250} width={250}></img>
+            <h2 className="card-title">Vivian</h2>
+            <p className="card-text">I like painting and I drink an insane amount of coffee</p>
+        </div>
+    );
+}
+
+export default Card
