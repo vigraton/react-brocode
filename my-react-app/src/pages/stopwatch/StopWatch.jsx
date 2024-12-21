c339605abfd6ad973c16f01264e560d50fbe2dba
vigraton <viviangraton@gmail.com>
2024-12-21 11:06:21 -0300

    stopwatch finished

--- my-react-app/src/pages/stopwatch/StopWatch.jsx
+++ my-react-app/src/pages/stopwatch/StopWatch.jsx
@@ -9,23 +9,45 @@ function StopWatch(){
     const startTimeRef = useRef(0);
 
     useEffect(() => {
+        if(isRunning){
+            intervalIdRef.current = setInterval(() => {
+                setElapsedTime(Date.now() - startTimeRef.current);
+            }, 10);
+        }
+
+        return () => {
+            clearInterval(intervalIdRef.current);
+        }
 
     }, [isRunning]);
 
     function start(){
-
+        setIsRunning(true);
+        startTimeRef.current = Date.now() - elapsedTime;
     }
 
     function stop(){
-
+        setIsRunning(false);
     }
 
     function reset(){
-
+        setElapsedTime(0);
+        setIsRunning(false);
     }
 
     function formatTime(){
-        return `00:00:00`;
+
+        let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+        let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+        let seconds = Math.floor(elapsedTime / (1000) % 60);
+        let milliseconds = Math.floor((elapsedTime % 1000) / 10);
+
+        hours = String(hours).padStart(2, "0");
+        minutes = String(minutes).padStart(2, "0");
+        seconds = String(seconds).padStart(2, "0");
+        milliseconds = String(milliseconds).padStart(2, "0");
+
+        return `${minutes}:${seconds}:${milliseconds}`;
     }
 
     return(
@@ -34,9 +56,9 @@ function StopWatch(){
             <div className="stopwatch">
             <div className="display">{formatTime()}</div>
             <div className="constrols">
-                <button onClick={start()} className="start-button">Start</button>
-                <button onClick={stop()} className="stop-button">Stop</button>
-                <button onClick={reset()} className="reset-button">Reset</button>
+                <button id="start-btn" onClick={start} className="start-button">Start</button>
+                <button id="stop-btn" onClick={stop} className="stop-button">Stop</button>
+                <button id="reset-btn" onClick={reset} className="reset-button">Reset</button>
             </div>
         </div>
         </div>
